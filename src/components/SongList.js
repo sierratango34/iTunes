968254f20f1f37e0468896f41879a662b5d3d4d6
sierratango34 <k.sritawan@gmail.com>
2019-07-27 12:57:27 +0100

    mapped details from song object in SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,10 +2,17 @@ import React from "react";
 import { SongDetail } from "./SongDetail";
 
 export const SongList = props => {
-  return (
-    <>
-      <p>this is a song list</p>
-      <SongDetail />
-    </>
-  );
+  const songs = props.songs.map(song => {
+    return (
+      <SongDetail
+        title={song["im:name"].label}
+        artist={song["im:artist"].label}
+        image={song["im:image"][2].label}
+        price={song["im:price"].label}
+        audioPreview={song.link[1].href}
+      />
+    );
+  });
+
+  return <>{songs}</>;
 };
