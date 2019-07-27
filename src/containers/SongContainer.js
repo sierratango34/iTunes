175fcd082db24d06acf5c91daa5f34a85bdea6e2
sierratango34 <k.sritawan@gmail.com>
2019-07-27 12:49:09 +0100

    componentDidMount in SongContainer props being passed are an array of 20 song objects

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -9,6 +9,13 @@ class SongContainer extends Component {
     };
   }
 
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+    fetch(url)
+      .then(res => res.json())
+      .then(songs => this.setState({ songs: songs.feed.entry }));
+  }
+
   render() {
     return (
       <>
