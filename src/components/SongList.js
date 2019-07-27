440122a2229adfb10205edee36b684db439ecb5c
sierratango34 <k.sritawan@gmail.com>
2019-07-27 12:43:06 +0100

    SongList file created

---
+++ src/components/SongList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SongList = props => {
+  return <p>this is a song list</p>;
+};
--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { SongList } from "../components/SongList";
 
 class SongContainer extends Component {
   render() {
-    return <h3>This is a song container</h3>;
+    return (
+      <>
+        <h3>This is a song container</h3>
+        <SongList />
+      </>
+    );
   }
 }
 
